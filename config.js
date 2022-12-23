4a0076a0b31f419943a385f818576943e9748a7c
Doobrazille2002 <121304129+Doobrazille2002@users.noreply.github.com>
2022-12-23 22:42:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['07040717622']  // -- Add multiple numbers like   ['08125925844']
+global.OwnerNumber =  ['07040717622'] // -- Add multiple numbers like  
+global.ownertag =  ['07040717622'] // -- Add multiple numbers like  ['08125925844']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
@@ -20,7 +20,7 @@ global.sessionName = "session"  // -- Don't change otherwise bot will not work -
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = ['🔥'] // Prefix ---  change it to anythong you want except '@' //
 
 
 global.location = "West Bengal, India"
